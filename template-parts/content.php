6dc2c77c92bea2cf745c327ed88d286e4f620def
Josh Bryant <kaytal@gmail.com>
2015-12-07 14:12:35 -0600

    Made a few styling changes to increase font on mobile for easier viewability.  Added instagram

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,11 +10,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header>
-		<h1 class="header-2"><?php echo $post->post_title ?></h1>
-	</header>
+	
 	<div class="row">
-		<div class="entry-content <?php if ((is_front_page()) || ($pagename == 'act-and-other-test-preparation') || ($pagename == 'home') || ($pagename == 'contact-us')) echo 'col-sm-8' ?>">
+		<div class="entry-content <?php if ((is_front_page()) || ($pagename== 'general-tutoring') || ($pagename == 'act-and-other-test-preparation') || ($pagename == 'home') || ($pagename == 'contact-us')) echo 'col-sm-8'; else echo 'col-sm-12' ?>">
+			<header>
+				<h1 class="header-2"><?php echo $post->post_title ?></h1>
+			</header>
 			<?php
 				the_content( sprintf(
 					/* translators: %s: Name of current post. */
@@ -30,6 +31,25 @@
 				) );
 			?>
 		</div><!-- .entry-content -->
+
+
+		<?php if ((is_front_page()) || ($pagename== 'general-tutoring') || ($pagename == 'act-and-other-test-preparation') || ($pagename == 'home') || ($pagename == 'contact-us')) : ?>
+		<div class="hours <?php if ((is_front_page()) || ($pagename== 'general-tutoring') || ($pagename == 'act-and-other-test-preparation') || ($pagename == 'home') || ($pagename == 'contact-us')) echo 'col-sm-4' ?>">
+			<h2 class="header-2">Hours:</h2>
+			<dl>
+				<dt>Monday - Thursday</dt>
+				<dd>3:00 PM - 9:00 PM</dd>
+				<dt>Friday</dt>
+				<dd>3:00 PM - 6:00 PM</dd>
+				<dt>Satruday</dt>
+				<dd>9:00 AM - 2:00 PM</dd>
+			</dl>
+			<p>* Some tutors also tutor away from the center in the student's home, local library or school.  Off campus tutoring charges an additional $10 traveling fee.</p>
+			<p>** Closed on major holidays</p>
+		</div>
+		<?php endif ?>
+
+
 	</div>
 
 	<footer class="entry-footer">
